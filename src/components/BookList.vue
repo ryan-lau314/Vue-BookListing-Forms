61cbdaa9a9fb2ad8976c9084fc4092e2fe322d31
Ryan Lau <ryan.lau@sensat.co.uk>
2020-07-17 10:08:18 +0100

    Filter the book list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,14 @@
     <ul>
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
+    <hr />
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters" :key="filter">{{filter}}</option>
+    </select>
+    <ul>
+      <book-item v-for="book in filteredBooks" :key="book.id" :book="book"></book-item>
+    </ul>
     <br />
     <hr />
     <book-form @addBook="appendBook"></book-form>
@@ -11,6 +19,8 @@
 </template>
 
 <script>
+import _ from "lodash";
+
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -24,19 +34,24 @@ export default {
         {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "bought"
         },
         {
           title: "Amusing Ourselves to Death",
           author: "Neil Postman",
-          finishedReading: false
+          finishedReading: false,
+          ownership: "borrowed"
         }
-      ]
+      ],
+      filters: ["bought", "borrowed"],
+      holding: "bought"
     };
   },
   components: {
@@ -48,8 +63,14 @@ export default {
       this.books.push({
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
-        finishedReading: bookData.finishedReading
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership
       });
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
     }
   }
 };
